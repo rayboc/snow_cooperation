1e686ed0433644e98463baf1616621aae6965cc7
rayboc <rayboc0803@gmail.com>
2021-04-27 11:27:03 +0900

    プロジェクト新規作成

---
+++ sn_cooperation/src/main/java/rayboc/sn_cooperation/SnCooperationApplication.java
@@ -0,0 +1,13 @@
+package rayboc.sn_cooperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnCooperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnCooperationApplication.class, args);
+	}
+
+}
---
+++ sn_cooperation/src/test/java/rayboc/sn_cooperation/SnCooperationApplicationTests.java
@@ -0,0 +1,13 @@
+package rayboc.sn_cooperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnCooperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
